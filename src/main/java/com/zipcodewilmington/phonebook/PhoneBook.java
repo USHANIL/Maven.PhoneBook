dfacdb7bcaedc22ea43d27950e1ca90f906b0101
ushanil <ush.gopi@gmail.com>
2019-11-13 18:02:48 -0500

    5 testcases passed..need to tweek on the order of names

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,45 +1,95 @@
 package com.zipcodewilmington.phonebook;
 
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBook {
+
+    Map <String, List<String>> mapPhone;
+    List<String> existingList;
+
     public PhoneBook(Map<String, List<String>> map) {
+        this.mapPhone = map;
     }
 
     public PhoneBook() {
-        this(null);
+
+        this.mapPhone = new HashMap<>();
+       // this(null);
     }
 
     public void add(String name, String phoneNumber) {
+
+
+       if (mapPhone.containsKey(name))
+       {
+           existingList = mapPhone.get(name);
+           existingList.add(phoneNumber);
+           mapPhone.replace(name, existingList);
+       }
+       else
+       {
+           existingList = new ArrayList<>();
+           existingList.add(phoneNumber);
+           mapPhone.put(name,existingList);
+       }
+
     }
 
     public void addAll(String name, String... phoneNumbers) {
+        if (mapPhone.containsKey(name))
+        {
+            existingList = mapPhone.get(name);
+            ArrayList ph = new ArrayList<String>(Arrays.asList(phoneNumbers));
+            existingList.addAll(ph);
+            mapPhone.replace(name, existingList);
+        }
+        else
+        {
+            existingList = new ArrayList<>();
+            ArrayList ph = new ArrayList<String>(Arrays.asList(phoneNumbers));
+            existingList.addAll(ph);
+            mapPhone.put(name,existingList);
+        }
     }
 
     public void remove(String name) {
+        mapPhone.remove(name);
     }
 
     public Boolean hasEntry(String name) {
-        return null;
+        return mapPhone.containsKey(name);
     }
 
     public List<String> lookup(String name) {
-        return null;
+        return mapPhone.get(name);
     }
 
-    public String reverseLookup(String phoneNumber)  {
-        return null;
+    public String reverseLookup(String phoneNumber) {
+        String nameofPhonenumber = "";
+        Iterator it = mapPhone.entrySet().iterator();
+        while(it.hasNext())
+        {
+            Map.Entry pair = (Map.Entry)it.next();
+            existingList  = (List<String>)pair.getValue();
+            if (existingList.contains(phoneNumber))
+            {
+                nameofPhonenumber = (String)pair.getKey();
+                break;
+            }
+        }
+        return nameofPhonenumber;
     }
 
     public List<String> getAllContactNames() {
-        return null;
+        Set nameSet = mapPhone.keySet();
+        existingList.addAll(nameSet);
+        return existingList;
     }
 
     public Map<String, List<String>> getMap() {
-        return null;
+        return mapPhone;
     }
 }
